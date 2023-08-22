05ca7dff36ddd3d284e31f0811dd1cb6c1b56b84
Juan Holy <juanholydesouza@gmail.com>
2023-08-22 14:15:01 -0400

    Payment, one-to-one association

---
+++ src/main/java/com/example/order/entities/Payment.java
@@ -0,0 +1,29 @@
+package com.example.order.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+}
